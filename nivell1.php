39201258dd668b500d70eb3ba3bfafdc0ae8f929
RubenCamus <Ruben-4-millet@hotmail.com>
2026-09-08 13:31:21 +0200

    feat: Nivell 1 exercici 2 done.

--- nivell1.php
+++ nivell1.php
@@ -5,12 +5,26 @@ $numero = 10;
 $numeroDecimal = 3.1415;
 $nom = "Pep";
 $veritat = true;
+$USER_NOM = "Ruben Camus";
+$holaMundo = "Hello, World!";
 
-echo $numero;
+// echo $numero;
+// echo PHP_EOL;
+// echo $numeroDecimal;
+// echo PHP_EOL;
+// echo $nom;
+// echo PHP_EOL;
+// echo $veritat;
+// echo PHP_EOL;
+// echo $USER_NOM;
+// echo PHP_EOL;
+
+echo $holaMundo;
+$holaMundoUpper = strtoupper($holaMundo);
 echo PHP_EOL;
-echo $numeroDecimal;
-echo PHP_EOL;
-echo $nom;
-echo PHP_EOL;
-echo $veritat;
+echo $holaMundoUpper, "\n";
+echo strlen($holaMundoUpper), "\n";
+echo strrev($holaMundoUpper), "\n";
+$cursString = "Aquest es el curs de PHP";
+echo "$holaMundoUpper $cursString";
 ?>
