4454a798f5c44dd116f2869e74dd6a76e5b74c08
RubenCamus <Ruben-4-millet@hotmail.com>
2026-09-08 13:11:07 +0200

    feat: Nivell 1 done

---
+++ nivell1.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types = 1);
+
+$numero = 10;
+$numeroDecimal = 3.1415;
+$nom = "Pep";
+$veritat = true;
+
+echo $numero;
+echo PHP_EOL;
+echo $numeroDecimal;
+echo PHP_EOL;
+echo $nom;
+echo PHP_EOL;
+echo $veritat;
+?>
