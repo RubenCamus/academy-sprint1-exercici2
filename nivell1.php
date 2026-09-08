66013d9eafd52ccae33ae2530a7e5f481692db31
RubenCamus <Ruben-4-millet@hotmail.com>
2026-09-08 13:43:48 +0200

    feat: Nivell 1 exercici 3 a)

--- nivell1.php
+++ nivell1.php
@@ -1,12 +1,12 @@
 <?php
 declare(strict_types = 1);
 
-$numero = 10;
-$numeroDecimal = 3.1415;
-$nom = "Pep";
-$veritat = true;
-$USER_NOM = "Ruben Camus";
-$holaMundo = "Hello, World!";
+// $numero = 10;
+// $numeroDecimal = 3.1415;
+// $nom = "Pep";
+// $veritat = true;
+// $USER_NOM = "Ruben Camus";
+// $holaMundo = "Hello, World!";
 
 // echo $numero;
 // echo PHP_EOL;
@@ -19,12 +19,38 @@ $holaMundo = "Hello, World!";
 // echo $USER_NOM;
 // echo PHP_EOL;
 
-echo $holaMundo;
-$holaMundoUpper = strtoupper($holaMundo);
-echo PHP_EOL;
-echo $holaMundoUpper, "\n";
-echo strlen($holaMundoUpper), "\n";
-echo strrev($holaMundoUpper), "\n";
-$cursString = "Aquest es el curs de PHP";
+// echo $holaMundo;
+// $holaMundoUpper = strtoupper($holaMundo);
+// echo PHP_EOL;
+// echo $holaMundoUpper, "\n";
+// echo strlen($holaMundoUpper), "\n";
+// echo strrev($holaMundoUpper), "\n";
+// $cursString = "Aquest es el curs de PHP";
 echo "$holaMundoUpper $cursString";
+
+$x = 10;
+$y = 90;
+$n = 140.12;
+$m = 3.6666;
+
+echo "x is $x y is $y";
+echo PHP_EOL;
+echo "sum is ", $x + $y;
+echo PHP_EOL;
+echo "minus is ", $x - $y;
+echo PHP_EOL;
+echo "product is ", $x * $y;
+echo PHP_EOL;
+echo "modul is ", $x / $y;
+echo PHP_EOL;
+echo PHP_EOL;
+echo "n is $n m is $m";
+echo PHP_EOL;
+echo "sum is ", $n + $m;
+echo PHP_EOL;
+echo "minus is ", $n - $m;
+echo PHP_EOL;
+echo "product is ", $n * $m;
+echo PHP_EOL;
+echo "modul is ", $n / $m;
 ?>
